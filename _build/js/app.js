ed9c30c5500c892a42ef500c78d4f3b4ec85bade
Zach SL <mzschandy@gmail.com>
2021-01-10 02:53:08 -0500

    hero image update

--- _build/js/app.js
+++ _build/js/app.js
@@ -1,1 +1,15 @@
-$(document).foundation();
+//$(document).foundation();
+
+window.onscroll = function() {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        console.log("change")
+        $("#navigation").addClass("scroll")
+    } else {
+        console.log("change back")
+        $("#navigation").removeClass("scroll")
+    }
+}
