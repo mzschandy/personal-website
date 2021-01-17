934673f43097612f71c05c7dad36b2b05077013e
Zach SL <mzschandy@gmail.com>
2021-01-16 20:39:32 -0500

    updated projects lister layout

--- _build/js/app.js
+++ _build/js/app.js
@@ -1,25 +1,24 @@
 //$(document).foundation();
 
-window.onscroll = function () {
-  scrollFunction();
+window.onscroll = function() {
+    scrollFunction();
 }
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    console.log("change")
-    $("#navigation").addClass("scroll")
-  } else {
-    console.log("change back")
-    $("#navigation").removeClass("scroll")
-  }
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        console.log("change")
+        $("#navigation").addClass("scroll")
+    } else {
+        console.log("change back")
+        $("#navigation").removeClass("scroll")
+    }
 }
 
-$(document).ready(function () {
-  $(".menuBtn").click(function () {
-    $("#mobileNav").show()
+$(document).ready(function() {
+    $(".menuBtn").click(function() {
+      $("#mobileNav").show()
+    })
+    $(".closeBtn").click(function() {
+      $("#mobileNav").hide()
+    })
   })
-  $(".closeBtn").click(function () {
-    $("#mobileNav").hide()
-  })
-})
-
