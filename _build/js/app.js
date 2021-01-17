0bbc9def6d8a7e39ca42568997a78c04cdd60201
Zach SL <mzschandy@gmail.com>
2021-01-16 19:13:11 -0500

    hero update

--- _build/js/app.js
+++ _build/js/app.js
@@ -1,24 +1,25 @@
 //$(document).foundation();
 
-window.onscroll = function() {
-    scrollFunction();
+window.onscroll = function () {
+  scrollFunction();
 }
 
 function scrollFunction() {
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        console.log("change")
-        $("#navigation").addClass("scroll")
-    } else {
-        console.log("change back")
-        $("#navigation").removeClass("scroll")
-    }
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    console.log("change")
+    $("#navigation").addClass("scroll")
+  } else {
+    console.log("change back")
+    $("#navigation").removeClass("scroll")
+  }
 }
 
-$(document).ready(function() {
-    $(".menuBtn").click(function() {
-      $("#mobileNav").show()
-    })
-    $(".closeBtn").click(function() {
-      $("#mobileNav").hide()
-    })
+$(document).ready(function () {
+  $(".menuBtn").click(function () {
+    $("#mobileNav").show()
   })
+  $(".closeBtn").click(function () {
+    $("#mobileNav").hide()
+  })
+})
+
